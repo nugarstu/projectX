48242f8895c6b0177f513d9caaab5c0b338b4b69
Muhammad Nuga Restu Fildanto <nugarstu@yahoo.com>
2015-09-07 15:22:49 +0700

    Create template.hs

---
+++ template.hs
@@ -0,0 +1,19 @@
+module Two where
+
+import Data.List
+
+x = 100
+
+koko = 69
+
+min' a b
+  | a < b = a
+  | otherwise = b
+
+max' a b
+  | a > b = a
+  | otherwise = b
+
+fst' (a, b) = a
+
+snd' (a, b) = b
